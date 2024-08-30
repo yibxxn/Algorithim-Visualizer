87aa1302edb89219ae9b66699b75406f6eaf6dc0
yibxxn <yibxn23@gmail.com>
2024-08-30 21:56:12 +0800

    Implemented buttons to randomize array
    Randomizes the array bars on click

--- src/components/Visualizer.tsx
+++ src/components/Visualizer.tsx
@@ -20,15 +20,54 @@ const [array, setArray] = useState<number[]>([]);
     setArray(array);
   };
 
+  const mergeSort = () =>{};
+  const quickSort = () =>{};
+  const heapSort = () =>{};
+  const bubbleSort = () =>{};
+
   return (
-    <div className="array-container">
-      {array.map((value, idx) => (
-        <div
-          className="array-bar"
-          key={idx}
-          style={{ height: `${value * 0.6}px` }}
-        ></div>
-      ))}   
+    <div className="array-container flex flex-col items-center">
+      <div className="bars-container">
+        {array.map((value, idx) => (
+          <div
+            className="array-bar"
+            key={idx}
+            style={{ height: `${value * 0.6}px` }}
+          ></div>
+        ))}
+      </div>
+      <div className="mt-4 flex space-x-4">
+      <button 
+        className="mt-4 bg-blue-400 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-500 hover:border-blue-400 rounded"
+        onClick={resetArray}
+      >
+        Generate New Array
+      </button>
+      <button 
+        className="mt-4 bg-blue-400 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-500 hover:border-blue-400 rounded"
+        onClick={resetArray}
+      >
+        Merge Sort
+      </button>
+      <button 
+        className="mt-4 bg-blue-400 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-500 hover:border-blue-400 rounded"
+        onClick={resetArray}
+      >
+        Qucik Sort
+      </button>
+      <button 
+        className="mt-4 bg-blue-400 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-500 hover:border-blue-400 rounded"
+        onClick={resetArray}
+      >
+        Heap Sort
+      </button>
+      <button 
+        className="mt-4 bg-blue-400 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-500 hover:border-blue-400 rounded"
+        onClick={resetArray}
+      >
+        Bubbule Sort
+      </button>
+    </div>
     </div>
   );
 };
